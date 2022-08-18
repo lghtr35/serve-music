44bee8fc1c3b95f8506d75e4ff5abdca204d22f0
serdil.cakmak <serdil.cakmak@trendyol.com>
2022-08-18 10:46:27 +0300

    Initial commit

---
+++ src/main/java/com/lghtr35/music/serve/ServeMusicApplication.java
@@ -0,0 +1,13 @@
+package com.lghtr35.music.serve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServeMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lghtr35/music/serve/ServeMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lghtr35.music.serve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
